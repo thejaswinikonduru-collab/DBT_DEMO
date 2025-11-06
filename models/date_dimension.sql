f62edb529eb66c2e7f3275a8f23a675970865fa3
TEJASWINI K <thejaswini.konduru@gmail.com>
2025-11-06 13:53:02 +0000

    daily_weather model created

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -3,15 +3,14 @@ SELECT
 to_timestamp(STARTED_AT) as STARTED_AT,
 date(to_timestamp(STARTED_AT)) as date_started_at,
 hour(to_timestamp(STARTED_AT)) as hour_started_at,
-case when
-dayname(to_timestamp(STARTED_AT)) in ('Sat','Sun') then 'weekend' else 'businessday' end as day_type,
-case when 
-month(to_timestamp(STARTED_AT)) in(12,1,2) then 'winter'
-when month(to_timestamp(STARTED_AT)) in(3,4,5) then 'spring'
-when month(to_timestamp(STARTED_AT)) in(6,7,8) then 'summer'
-else 'autumn' end as station_year
+
+    {{day_type('STARTED_AT')}} as day_type,
+
+    {{get_season('STARTED_AT')}} as station_year
+        
+
     from {{ source('demo', 'bike') }}
-    where STARTED_AT != 'started_at'
-)
+    where STARTED_AT != 'started_at')
+
 
 select * from CTE
