8acaf5db72f45d34181302e3788d0933e90465f9
TEJASWINI K <thejaswini.konduru@gmail.com>
2025-11-06 14:52:40 +0000

    daily_weather model updated

--- models/daily_weather.sql
+++ models/daily_weather.sql
@@ -1,32 +1,41 @@
-with daily_weather as(
+WITH daily_weather as (
 
-    select 
-    date(time) as daily_weather,
-    weather,
-    temp,
-    pressure,
-    humidity,
-    clouds
 
-    from 
-    {{ source('demo', 'weather') }}
+select
 
-    
+date(time) as daily_weather,
+weather,
+temp,
+pressure,
+humidity,
+clouds
+
+from {{ source('demo', 'weather') }}
+
+
+
 ),
 
-daily_weather_agg as(
+daily_weather_agg as (
 
-    select daily_weather,
-    weather,
-    round(avg(temp),2) as avg_temp,
-    round(avg(pressure),2) as avg_pressure,
-    round(avg(humidity),2) as avg_humidity,
-    round(avg(clouds),2) as avg_clouds,
-   
+select
+daily_weather,
+weather,
+round(avg(temp),2) as avg_temp,
+round(avg(pressure),2) as avg_pressure,
+round(avg(humidity),2) as avg_humidity,
+round(avg(clouds),2) as avg_clouds
 
-    from daily_weather
-    group by daily_weather,weather)
+from daily_weather
 
-    
+group by daily_weather, weather
 
-select * from daily_weather_agg
+qualify ROW_NUMBER() OVER (PARTITION BY daily_weather ORDER BY count(weather) desc) =1
+
+)
+
+
+
+select
+*
+from daily_weather_agg
